4d896027f643011eed15ca881f1c3a466b41b8ad
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-06-08 17:37:43 -0400

    refactoring
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- src/handlerEntry.js
+++ src/handlerEntry.js
@@ -1,7 +1,3 @@
-import { sqliteTable } from "drizzle-orm/sqlite-core";
-import { jsonb, timestamp, varchar } from "drizzle-orm/pg-core";
-import { drizzle } from "drizzle-orm/d1";
-import { eq } from "drizzle-orm";
 import {
   handleGet,
   handlePost,
@@ -71,12 +67,6 @@ export async function handleRequest(request, env, context) {
       }
     );
   }
-  
-  const user_preferences = sqliteTable("user_preferences", {
-    account_id: varchar("account_id").notNull().primaryKey(),
-    preferences: jsonb("preferences").notNull(),
-    last_update_datetime: timestamp("last_update_datetime").notNull(),
-  });
 
   var responseHeaders = {
     "Access-Control-Allow-Credentials": "true",
@@ -108,23 +98,6 @@ export async function handleRequest(request, env, context) {
         },
       }
     );
-  }
-
-  const db = drizzle(env.user_prefs_database);
-  try {
-    await db
-      .select()
-      .from(user_preferences)
-      .where(eq(user_preferences.account_id, accountResponse["id"]));
-  } catch (error) {
-    await env.user_prefs_database
-      .prepare(
-        `CREATE TABLE user_preferences (
-      account_id varchar(64) PRIMARY KEY,
-      preferences jsonb,
-      last_update_datetime timestamp)`
-      )
-      .run();
   }
 
   var responseObject = {};
