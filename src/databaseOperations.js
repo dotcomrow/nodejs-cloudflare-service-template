9eb1d368f830bd00a47cc1a862bbc409e6a8c706
mephmanx <52037738+mephmanx@users.noreply.github.com>
2024-06-09 14:40:09 +0000

    Deploying to release from @ dotcomrow/nodejs-cloudflare-service-template@86fe5d9646d77b7608a025ea66aed77f895fa295 🚀

---
+++ src/databaseOperations.js
@@ -0,0 +1,28 @@
+import { sqliteTable } from "drizzle-orm/sqlite-core";
+import { jsonb, timestamp, varchar } from "drizzle-orm/pg-core";
+
+export const user_preferences = sqliteTable("user_preferences", {
+    account_id: varchar("account_id").notNull().primaryKey(),
+    preferences: jsonb("preferences").notNull(),
+    last_update_datetime: timestamp("last_update_datetime").notNull(),
+  });
+  
+export async function dbSetup(env, account_id) {
+  
+    const db = drizzle(env.user_prefs_database);
+    try {
+      await db
+        .select()
+        .from(user_preferences)
+        .where(eq(user_preferences.account_id, account_id));
+    } catch (error) {
+      await env.user_prefs_database
+        .prepare(
+          `CREATE TABLE user_preferences (
+        account_id varchar(64) PRIMARY KEY,
+        preferences jsonb,
+        last_update_datetime timestamp)`
+        )
+        .run();
+    }
+}
