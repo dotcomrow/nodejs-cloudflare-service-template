ad0778c0d1be3b6fd873b943d70df618e3d2ab4d
mephmanx <52037738+mephmanx@users.noreply.github.com>
2024-06-17 21:30:08 +0000

    Deploying to release from @ dotcomrow/nodejs-cloudflare-service-template@0437909c6b2a8a8f15c4fd843714c78dd788a879 🚀

---
+++ variables.tf
@@ -0,0 +1,35 @@
+variable "cloudflare_account_id" {
+  description = "Cloudflare account id"
+  type        = string
+  nullable = false
+}
+
+variable "domain" {
+  description = "domain"
+  type        = string
+  nullable = false
+}
+
+variable "cloudflare_zone_id" {
+  description = "cloudflare worker zone id"
+  type        = string
+  nullable = false
+}
+
+variable "user_profile_svc_endpoint" {
+  description = "user profile svc url"
+  type        = string
+  nullable = false
+}
+
+variable "cloudflare_token" {
+  description = "cloudflare token"
+  type        = string
+  nullable = false
+}
+
+variable init_key {
+  description = "initialization key"
+  type        = string
+  nullable = false
+}
