0e120ea17a810c0cad4d3a0ef0c73a2231eb5da8
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-06-21 17:49:14 -0400

    removing
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,25 +1,8 @@
 import { handleRequest } from "./handlerEntry.js";
 import { serializeError } from "serialize-error";
-import {Logging} from '@google-cloud/logging';
 
 export default {
   async fetch(request, env, context) {
-    const logging = new Logging();
-    const logName = await env.LOG_NAME;
-    const log = logging.log(logName);
-
-    const metadata = {
-      severity: 'DEBUG',
-      resource: {
-        type: 'global',
-      },
-    };
-  
-    const message = {
-      name: 'Worker ' + env.WORKER_NAME,
-      information: "Request received",
-    };
-    log.entry(metadata, message);
     try {
       return await handleRequest(request, env, context);
     } catch (e) {
