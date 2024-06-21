7869ef694860666bbe0c50380de932edabefa447
mephmanx <52037738+mephmanx@users.noreply.github.com>
2024-06-21 19:46:20 +0000

    Deploying to release from @ dotcomrow/nodejs-cloudflare-service-template@cab103afaed8a7fd6a4a0fde14ea3fdf82389586 🚀

---
+++ src/handlerEntry.js
@@ -0,0 +1,169 @@
+import {
+  handleGet,
+  handlePost,
+  handlePut,
+  handleDelete,
+} from "./requestHandlers.js";
+import { dbSetup } from "./databaseOperations.js";
+
+export async function handleRequest(request, env, context) {
+  var origin = request.headers.get("Origin") || request.headers.get("origin");
+
+  if (request.method === "OPTIONS") {
+    const cors_domains = await env.CORS_DOMAINS.split(",");
+    var originAllowed = false;
+    for (var d in cors_domains) {
+      var regex = new RegExp(cors_domains[d]);
+      if (regex.test(origin)) {
+        originAllowed = true;
+      }
+    }
+    if (!originAllowed) {
+      return new Response(
+        JSON.stringify({ message: "CORS Not supported -> " + origin }),
+        {
+          status: 403,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    }
+    return new Response("", {
+      status: 204,
+      headers: {
+        "Access-Control-Allow-Credentials": "true",
+        "Access-Control-Allow-Origin": origin,
+        "Access-Control-Allow-Methods": "POST, GET, OPTIONS, DELETE, PUT",
+        "Access-Control-Allow-Headers": "Authorization, Content-Type, Identity",
+        Connection: request.headers.get("Connection"),
+      },
+    });
+  }
+
+  if (request.method == "HEAD") {
+    if (
+      new String(request.url.substring(request.url.lastIndexOf('/') + 1)).valueOf() ==
+      new String(env.INITIALIZATION_KEY).valueOf()
+    ) {
+      await dbSetup(env);
+      return new Response("", {
+        status: 200,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+    } else {
+      return new Response("",
+        {
+          status: 403,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    }
+  }
+
+  var authHeader = "";
+  if (
+    request.headers.get("Authorization") != undefined ||
+    request.headers.get("authorization") != undefined
+  ) {
+    if (
+      request.headers.get("Authorization") != undefined &&
+      request.headers.get("Authorization").startsWith("Bearer ")
+    ) {
+      authHeader = request.headers.get("Authorization").split(" ")[1];
+    } else if (
+      request.headers.get("authorization") != undefined &&
+      request.headers.get("authorization").startsWith("Bearer ")
+    ) {
+      authHeader = request.headers.get("authorization").split(" ")[1];
+    }
+  } else {
+    return new Response(
+      JSON.stringify({ message: "Authorization header not found." }),
+      {
+        status: 403,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
+
+  var responseHeaders = {
+    "Access-Control-Allow-Credentials": "true",
+    "Access-Control-Allow-Origin": origin,
+    "Access-Control-Allow-Methods": "POST, GET, OPTIONS, DELETE, PUT",
+    "Access-Control-Allow-Headers": "Authorization, Content-Type, Identity",
+    Connection: request.headers.get("Connection"),
+    "Content-Type": "application/json",
+  };
+
+  const googleProfileUrl =
+    "https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=" +
+    authHeader;
+
+  var response = await fetch(googleProfileUrl, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  var accountResponse = JSON.parse(await response.text());
+  if (accountResponse == undefined || accountResponse["id"] == undefined) {
+    return new Response(
+      JSON.stringify({ message: "Account not found / token invalid." }),
+      {
+        status: 403,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
+
+  var responseObject = {};
+  switch (request.method) {
+    case "GET":
+      responseObject = await handleGet(
+        env,
+        accountResponse["id"],
+        request.headers.get("Identity")
+      );
+      break;
+    case "PUT":
+      var bodyObj = await request.json();
+      responseObject = await handlePut(
+        env,
+        accountResponse["id"],
+        bodyObj,
+        request.headers.get("Identity")
+      );
+      break;
+    case "POST":
+      var bodyObj = await request.json();
+      responseObject = await handlePost(
+        env,
+        accountResponse["id"],
+        bodyObj,
+        request.headers.get("Identity")
+      );
+      break;
+    case "DELETE":
+      responseObject = await handleDelete(
+        env,
+        accountResponse["id"],
+        request.headers.get("Identity")
+      );
+      break;
+  }
+
+  return new Response(JSON.stringify(responseObject), {
+    status: 200,
+    headers: responseHeaders,
+  });
+}
