1b02a503f5f22fa6178126a9e2851aee47ed5f93
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-06-23 15:33:51 -0400

    another update
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -5,16 +5,18 @@ import GCloudLogger from "npm-gcp-logging"
 export default {
   async fetch(request, env, context) {
     try {
-      const projectId = 'gcploggingproject-427121'; // replace with your GCP project ID  
-      
-      const logName = 'my-log';
-      const severity = 'ERROR';
-      const message = 'This is a log message.';
-      GCloudLogger.default.logEntry(projectId, env.GCP_LOGGING_CREDENTIALS,logName, severity, message);
-
       return await handleRequest(request, env, context);
     } catch (e) {
       const responseError = serializeError(e);
+      GCloudLogger.default.logEntry(env.GCP_LOGGING_PROJECT_ID, env.GCP_LOGGING_CREDENTIALS,env.LOG_NAME, 
+        [
+          {
+            severity: "ERROR",
+            // textPayload: message,
+            jsonPayload: responseError
+          },
+        ]
+      );
       return new Response(JSON.stringify(responseError), {
         status: 500,
         headers: {
