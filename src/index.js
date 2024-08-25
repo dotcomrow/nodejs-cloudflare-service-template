8991cf6ca115fb12f36f090c687ddd7c89a4162c
mephmanx <52037738+mephmanx@users.noreply.github.com>
2024-08-25 20:14:41 +0000

    Deploying to release-dev from @ dotcomrow/nodejs-cloudflare-service-template@ca8ba7580c9e9a6e77f452ad5f5dfe4f1176ddc0 🚀

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import { handleRequest } from "./handlerEntry.js";
+import { serializeError } from "serialize-error";
+import { default as LogUtility } from "./utils/LoggingUtility.js";
+
+export default {
+  async fetch(request, env, context) {
+    self.location = new URL("https://www.google.com");
+    var loggingContext = await LogUtility.buildLogContext(env);
+    try {
+      if (request.headers.get("X-Shared-Secret") == env.GLOBAL_SHARED_SECRET) {
+        return await handleRequest(request, env, context);
+      } else {
+        return new Response(JSON.stringify({
+          message: "Unauthorized"
+        }), {
+          status: 401,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+      }
+    } catch (e) {
+      const responseError = serializeError(e);
+      await LogUtility.logEntry(loggingContext, [
+        {
+          severity: "ERROR",
+          jsonPayload: {
+            message: "Exception occurred in fetch",
+            error: responseError,
+          },
+        },
+      ]);
+      return new Response(JSON.stringify(responseError), {
+        status: 500,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+    }
+  },
+};
